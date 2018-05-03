2aae826aa768c4c5afc6e4e96467363b649b49b5
JasonSLS <jason@esilverliningsolutions.com>
2018-05-03 12:48:53 -0700

    Create CTRC;~!~!~!~.js

---
+++ Scripts/Event/CTRC;~!~!~!~.js
@@ -0,0 +1,10 @@
+//for CTRC/*/*/*:
+
+//Needed to get GIS feature associated when created by ACA
+//Copy Parcel GIS Objects to Record using function copyParcelGisObjects()
+try{
+      copyParcelGisObjects();
+} catch (err) {
+      logDebug("A JavaScript Error occurred: CTRCA:*/*/*/*: copyParcelGisObjects()" + err.message);
+      logDebug(err.stack);
+};
