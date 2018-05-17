253aa1d090b57b3097634d2252b2ebc2ed6b4dc9
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-05-17 17:05:03 -0600

    Update ASA;~!~!~!~.js
    updating to test git

--- Scripts/Event/ASA;~!~!~!~.js
+++ Scripts/Event/ASA;~!~!~!~.js
@@ -1,11 +1,35 @@
-//For ASA/*/*/*:
+if (matches(currentUserID,"JJACKSON","ADMIN")) {
+	showDebug = 3;
+	showMessage= true;
+}
+// call CSLB Interface for LP
+try {
+	var newLicNum = false; 
+	newLicNum = getLPLicNum(capId);
+	var newLicType = false; 
+	newLicType = getLPLicType(capId);
+	var conType = false;
+	if(matches(newLicType,"Contractor","Electrical","Plumbing","Mechanical")) {
+		conType = true;
+	}
 
+	var cslbMessage = null;
+	cslbMessage = externalLP_CA_AT(newLicNum,newLicType,true,true,capId);
+	if (conType && cslbMessage) {
+		showMessage = true;
+		comment(cslbMessage);
+	}
+
+}
+catch (err) {
+	logDebug("A JavaScript Error occurred: ASA;DS~!~!~!~ - CSLB Interface" + err.message);
+} ;
 //Copy Parcel GIS Objects to Record using function copyParcelGisObjects()
-try{
-      if(!publicUser){
-            copyParcelGisObjects();
-      }
-} catch (err) {
-      logDebug("A JavaScript Error occurred: ASA:*/*/*/*: copyParcelGisObjects()" + err.message);
-      logDebug(err.stack);
-};
+// try{
+//      if(!publicUser){
+//            copyParcelGisObjects();
+//      }
+//} catch (err) {
+//      logDebug("A JavaScript Error occurred: ASA:*/*/*/*: copyParcelGisObjects()" + err.message);
+//      logDebug(err.stack);
+//};
