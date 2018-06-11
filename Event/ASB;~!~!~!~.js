10c19ded82f0f87fdb33b2489bd9941713a4a07b
JasonSLS <jason@esilverliningsolutions.com>
2018-06-11 15:03:42 -0700

    Update ASB;~!~!~!~.js

--- Event/ASB;~!~!~!~.js
+++ Event/ASB;~!~!~!~.js
@@ -1,3 +1,11 @@
+/*------------------------------------------------------------------------------------------------------/
+| Program: ASB:*/*/*/*.js  Trigger: Application Submit Before
+| Client:Santa Barbara - Silver Lining Solutions
+|
+| Version 1.0 - Base Version. 
+|
+/------------------------------------------------------------------------------------------------------*/
+
 if (matches(currentUserID,"JJACKSON","ADMIN")) {
 	showDebug = 3;
 	showMessage= true;
