4442e677e98719641da51703d9e85bbeaeb83fb7
JasonSLS <jason@esilverliningsolutions.com>
2018-06-11 16:01:32 -0700

    Update ASB;~!~!~!~.js

--- Event/ASB;~!~!~!~.js
+++ Event/ASB;~!~!~!~.js
@@ -10,7 +10,11 @@ if (matches(currentUserID,"JJACKSON","ADMIN")) {
 	showDebug = 3;
 	showMessage= true;
 }
+//Proximity Alert for High Fire Hazard Areas
+var vIsWithinProximity = proximity("SANTABARBARA","High Fire Hazard Areas","0","feet");
 
-//Start Proximity Alerts
-include("High_Fire_Hazard_Areas");
-//End Proximuty Alerts
+if (vIsWithinProximity == true) {
+		showMessage = true;
+		//cancel = true; 
+		comment("<B><Font Color=RED>Please be advised, this property is within a High Fire Hazard Area</B></Font>.");
+}
