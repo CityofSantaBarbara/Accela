b53ec4f1c2233b0d7ee1e43d253553e17bee7ae5
JasonSLS <jason@esilverliningsolutions.com>
2018-06-11 15:57:34 -0700

    Update High_Fire_Hazard_Areas.js

--- PROXIMITY_ALERTS/High_Fire_Hazard_Areas.js
+++ PROXIMITY_ALERTS/High_Fire_Hazard_Areas.js
@@ -1,1 +1,8 @@
-Test
+//Proximity Alert for High Fire Hazard Areas
+var vIsWithinProximity = proximity("SANTABARBARA","High Fire Hazard Areas","0","feet");
+
+if (vIsWithinProximity == true) {
+		showMessage = true;
+		//cancel = true; 
+		comment("<B><Font Color=RED>Please be advised, this property is within a High Fire Hazard Area</B></Font>.");
+}
