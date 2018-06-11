b113c2c7527b49abc0238b3f4008ec73fb182bf3
JasonSLS <jason@esilverliningsolutions.com>
2018-06-11 15:04:19 -0700

    Update CTRC;~!~!~!~.js

--- Event/CTRC;~!~!~!~.js
+++ Event/CTRC;~!~!~!~.js
@@ -1,5 +1,10 @@
-//for CTRC/*/*/*:
-
+/*------------------------------------------------------------------------------------------------------/
+| Program: CTRC:*/*/*/*.js  Trigger: Create to Real Cap
+| Client:Santa Barbara - Silver Lining Solutions
+|
+| Version 1.0 - Base Version. 
+|
+/------------------------------------------------------------------------------------------------------*/
 //Needed to get GIS feature associated when created by ACA
 //Copy Parcel GIS Objects to Record using function copyParcelGisObjects()
 try{
