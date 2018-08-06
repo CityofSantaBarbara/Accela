0ee2de81dde390cd4fef573a361e3c309467900c
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-08-06 17:37:16 -0600

    Create IRSA;Fire!Sprinkler System!Commercial - NFPA 13!NA.js
    initial draft

---
+++ Event/IRSA;Fire!Sprinkler System!Commercial - NFPA 13!NA.js
@@ -0,0 +1,19 @@
+/* 
+IRSA: Fire!Sprinkler System!Commercial - NFPA 13!NA 
+
+changes:
+
+01.01.2018 - this is note about a change
+
+*/
+
+
+aa.print("start of IRSA:Fire!Sprinkler System!Commercial - NFPA 13!NA");
+
+aa.print("hello world");
+
+cancel=true;
+
+aa.print("canceled it!");
+
+aa.print("end of IRSA:Fire!Sprinkler System!Commercial - NFPA 13!NA");
