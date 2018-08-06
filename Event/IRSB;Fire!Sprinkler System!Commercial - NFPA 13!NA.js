92549a55c12ea4b2ff72e10a21603546ed2d3dfd
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-08-06 16:26:53 -0600

    Create IRSB;Fire!Sprinkler System!Commercial - NFPA 13!NA.js
    initial draft

---
+++ Event/IRSB;Fire!Sprinkler System!Commercial - NFPA 13!NA.js
@@ -0,0 +1,9 @@
+aa.print("start of IRSB:Fire!Sprinkler System!Commercial - NFPA 13!NA");
+
+aa.print("hello world");
+
+cancel=true;
+
+aa.print("cancelled it!");
+
+aa.print("end of IRSB:Fire!Sprinkler System!Commercial - NFPA 13!NA");
