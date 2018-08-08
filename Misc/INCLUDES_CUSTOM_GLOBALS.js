a91cf6fab33e32eda8d2c6a6d51a8f6521fe1280
SBAccela <mknight@santabarbaraca.gov>
2018-08-08 17:18:49 -0600

    commit by emse tools 2018-08-08 16:18:45

--- Misc/INCLUDES_CUSTOM_GLOBALS.js
+++ Misc/INCLUDES_CUSTOM_GLOBALS.js
@@ -1,1 +1,24 @@
-if (currentUserID == "ADMIN") showDebug = 3;
+/*------------------------------------------------------------------------------------------------------/
+| Program : INCLUDES_CUSTOM_GLOBALS.js
+| Event   : N/A
+|
+| Usage   : Accela Custom Includes.  Required for all Custom Parameters
+|
+| Notes   : 
+|
+/------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| Custom Parameters
+|	Ifchanges are made, please add notes above.
+/------------------------------------------------------------------------------------------------------*/
+feeEstimate=false;
+if(vEventName.equals("FeeEstimateAfter4ACA")) 
+	feeEstimate=true;
+/*------------------------------------------------------------------------------------------------------/
+| END Custom Parameters
+/------------------------------------------------------------------------------------------------------*/
+
+if (matches(currentUserID, "ADMIN", "JJACKSON", "CWEIFFENBACH", "EKOONTZ")) {
+showDebug = 3;
+showMessage = true;
+}
