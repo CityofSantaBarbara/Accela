82f5cd3ecdc8aa8dd5b0c884393d4e1027eb5ddc
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-08-08 10:59:30 -0600

    Add files via upload

--- Event/WTUB;~!~!~!~.js
+++ Event/WTUB;~!~!~!~.js
@@ -1,4 +1,19 @@
 // SAMPLE SCRIPT TO MAP GIS INFO TO AN ASI AND GET PROX ALERT!
 showMessage = true;
-logDebug("Script WTUB - ");
+logDebug("Script WTUB - Begin");
+logDebug("BalanceDue = " + balanceDue);
 
+var lookupString = "appTypeString" + "|" + wfTask + "|" + wfStatus;
+logDebug("lookupString = " + lookupString);
+
+var lookupValue = lookup("ValidationZeroBalance", lookupString);
+logDebug("lookupValue = " + lookupValue);
+
+if (lookupValue)
+{
+	logDebug("lookupValue matched in ValidationZeroBalance table");
+	comment("This Workflow Action may not be completed while there is a Balance on the Record.");
+	cancel = true;
+}
+
+logDebug("Script WTUB - End");
