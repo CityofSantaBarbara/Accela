c237fe0bd735682fe502b75970b521942f33f0f7
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-08-08 15:03:36 -0600

    Update IRSB;Fire!Sprinkler System!Commercial - NFPA 13!NA.js
    took example code out

--- Event/IRSB;Fire!Sprinkler System!Commercial - NFPA 13!NA.js
+++ Event/IRSB;Fire!Sprinkler System!Commercial - NFPA 13!NA.js
@@ -1,9 +1,3 @@
-aa.print("start of IRSB:Fire!Sprinkler System!Commercial - NFPA 13!NA");
+logDebug("start of IRSB:Fire!Sprinkler System!Commercial - NFPA 13!NA");
 
-aa.print("hello world");
-
-cancel=true;
-
-aa.print("cancelled it!");
-
-aa.print("end of IRSB:Fire!Sprinkler System!Commercial - NFPA 13!NA");
+logDebug("end of IRSB:Fire!Sprinkler System!Commercial - NFPA 13!NA");
