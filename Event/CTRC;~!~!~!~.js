ebbcea4d90347046aa3629368133a0d89eb6c6d5
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-08-08 17:11:58 -0600

    Update CTRC;~!~!~!~.js
    change slash stars

--- Event/CTRC;~!~!~!~.js
+++ Event/CTRC;~!~!~!~.js
@@ -20,6 +20,6 @@
 try{
       copyParcelGisObjects();
 } catch (err) {
-      logDebug("A JavaScript Error occurred: CTRCA:*/*/*/*: copyParcelGisObjects()" + err.message);
+      logDebug("A JavaScript Error occurred: CTRCA:~/~/~/~: copyParcelGisObjects()" + err.message);
       logDebug(err.stack);
 };
