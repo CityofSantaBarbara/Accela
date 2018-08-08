a2e6060ba52a9a2e5caeec9f1e5250e73641afb2
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-08-08 16:36:28 -0600

    Update IRSA;Fire!Sprinkler System!Commercial - NFPA 13!NA.js
    changed to look for specific status and then close workflow task

--- Event/IRSA;Fire!Sprinkler System!Commercial - NFPA 13!NA.js
+++ Event/IRSA;Fire!Sprinkler System!Commercial - NFPA 13!NA.js
@@ -1,19 +1,35 @@
-/* 
-IRSA: Fire!Sprinkler System!Commercial - NFPA 13!NA 
+//********************************************************************************************************
+//Script 		InspectionResultSubmitAfter
+//Record Types:	Fire!Sprinkler System!Commercial - NFPA 13!NA 
+//
+//Event: 		
+//
+//Desc:			Use this script to move the record status and close the entire 
+//          record if final inspection complete
+//
+//Created By: Silver Lining Solutions
+//********************************************************************************************************
+// Change Log
+//         		Date		Name			Modification
+//				08-04-2018	Chad			Initial Draft
+//				08-08-2018	Chad			changed to look for specific status and then close workflow task
+//********************************************************************************************************
 
-changes:
 
-01.01.2018 - this is note about a change
+logDebug("start of IRSA:Fire!Sprinkler System!Commercial - NFPA 13!NA");
 
-*/
+if (InspectionType == "Final" && InspectionResult == "Passed" ) {
+	var donotCloseRecord = checkSprinklerHeadAndCancel();
+	cancel = false; // need to reset this so the event doesn't cancel because of the checkSprinklerHeadAndCancel call
+	 
+	if (donotCloseRecord) {
+		// there is a problem, so you don't want to close anything yet.
+	} else {
+		logDebug("IRSA:Fire!Sprinkler System!Commercial - NFPA 13!NA - final inspection complete, closing inspection workflow task")
+		closeTask("Inspection","Final Inspection Complete", "Final inspection completed, closed by script");
+		
+		// you could expand this to check for balance due and if none close the whole workflow and record.
+	}
+}
 
-
-aa.print("start of IRSA:Fire!Sprinkler System!Commercial - NFPA 13!NA");
-
-aa.print("hello world");
-
-cancel=true;
-
-aa.print("canceled it!");
-
-aa.print("end of IRSA:Fire!Sprinkler System!Commercial - NFPA 13!NA");
+logDebug("end of IRSA:Fire!Sprinkler System!Commercial - NFPA 13!NA");
