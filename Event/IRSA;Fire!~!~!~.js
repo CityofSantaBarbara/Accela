1e19a0986c8b56f827014b7dc4469bcb85c470f5
wallybot12 <42388330+wallybot12@users.noreply.github.com>
2018-08-14 18:44:32 +0000

    First Draft

---
+++ Event/IRSA;Fire!~!~!~.js
@@ -0,0 +1,32 @@
+//********************************************************************************************************
+//Script 		73 FIR - Close out cases 
+//Record Types:	Fire!~!~!~ 
+//
+//Event: 		InspectionResultSubmitAfter
+//
+//Desc:			Details for the scrip requirements identified with group webex: 
+//              Trigger: Inspection Result After 
+//              Criteria Record Types: All Fire Records – no exceptions Inspection Group: Inspection Types: ‘Fire Final’ 
+//				Inspection Result: ‘Passed’ 
+//				Action Close Workflow Task Task = ‘Inspection’ 
+//				Status = ‘Final Inspection Passed’ 
+//				Task = ‘Close’ Status = ‘Closed’ Close Record Application Status = ‘Closed’
+//
+//Created By: ASH 8-14-2018
+//********************************************************************************************************
+// Change Log
+//         		Date		Name			Modification
+//				08-14-2018	Alec			Initial Draft
+//********************************************************************************************************
+
+
+logDebug("start of IRSA:Fire!~!~!~");
+
+
+logDebug("The Middle of the Script");
+
+
+
+
+
+logDebug("end of IRSA:Fire!~!~!~");
