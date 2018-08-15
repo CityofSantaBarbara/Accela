294ca44b506218a7e205d3f225d29af93f04a63d
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-08-15 12:55:01 -0600

    Add files via upload

---
+++ Event/DUA;~!~!~!~.js
@@ -0,0 +1,20 @@
+//********************************************************************************************************
+//Script 		Email Staff on Document Update
+//Record Types:	​*/*/*/*
+//
+//Event: 		DUA
+//
+//Desc:			When a Revision Required Document gets resubmitted thru ACA. Email the Santa Barbara Staff 
+// 				that is assigned to the Plan Distributed Workflow Task. Also set the Workflow Task Plans 
+//				Distribution Status to Revisions Received.
+//
+//Created By: Silver Lining Solutions 
+//********************************************************************************************************
+// Change Log
+//         		Date		Name			Modification
+//				08/15/2018	Eric 			Initial Development
+//********************************************************************************************************
+logDebug("Script 31 Email Staff on Document Update - Begin");
+
+
+logDebug("Script 31 Email Staff on Document Update - End");
