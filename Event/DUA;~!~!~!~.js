d7e385e00973f5f720af7896d908552bf8ff1346
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-08-15 15:10:45 -0600

    Add files via upload

--- Event/DUA;~!~!~!~.js
+++ Event/DUA;~!~!~!~.js
@@ -8,6 +8,13 @@
 // 				that is assigned to the Plan Distributed Workflow Task. Also set the Workflow Task Plans 
 //				Distribution Status to Revisions Received.
 //
+//Assumptions:
+//				Staff must always be assigned to Plans Distribution 
+//				Staff must have a valid email defined in their User Profile
+//
+//Psuedo Code:	
+// 				use Document Update Notification template
+//
 //Created By: Silver Lining Solutions 
 //********************************************************************************************************
 // Change Log
@@ -16,5 +23,29 @@
 //********************************************************************************************************
 logDebug("Script 31 Email Staff on Document Update - Begin");
 
+activateTask("Plans Distribution");
+taskStatus("Plans Distribution","Revisions Received");
+
+// prepare Notification parameters
+var staff = getTaskAssignedStaff();
+var fromEmail = "noreply@SantaBarbaraCA.gov";
+var toEmail = staff.getEmail();
+var ccEmail = "eric@esilverliningsolutions.com";
+var notificationTemplate = "DOCUMENT UPDATE";
+var reportFile = [];  // empty set for the file list
+var capID4Email = aa.cap.createCapIDScriptModel(capId.getID1(),capId.getID2(),capId.getID3());
+var emailParameters = aa.util.newHashtable();
+
+addParameter(emailParameters, "$$altID$$", cap.getCapModel().getAltID());
+addParameter(emailParameters, "$$recordAlias$$", cap.getCapType().getAlias());
+
+logDebug("documentModelArray = " + documentModelArray);
+
+// send Notification
+var sendResult = sendNotification(fromEmail,toEmail,ccEmail,notificationTemplate,emailParameters,reportFile,capID4Email);
+if (!sendResult) 
+	{ logDebug("UNABLE TO SEND NOTICE!  ERROR: "+sendResult); }
+else
+	{ logDebug("Sent Notification"); }  
 
 logDebug("Script 31 Email Staff on Document Update - End");
