b12173fa4a695dbadddb082566b2e75fa4c7fc19
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-08-20 17:21:07 -0600

    Update CTRCA;~!~!~!~.js
    took out sumFeesAssessedBeforeInvoiceAndAddTechFee

--- Event/CTRCA;~!~!~!~.js
+++ Event/CTRCA;~!~!~!~.js
@@ -23,5 +23,3 @@ try{
       logDebug("A JavaScript Error occurred: CTRCA:~/~/~/~: copyParcelGisObjects()" + err.message);
       logDebug(err.stack);
 };
-
-sumFeesAssessedBeforeInvoiceAndAddTechFee ();
