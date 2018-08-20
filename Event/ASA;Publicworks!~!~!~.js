7d397ac56ee8b6c39804bac83359bf66a3ec0972
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-08-20 17:03:05 -0600

    Add files via upload
    initial

---
+++ Event/ASA;Publicworks!~!~!~.js
@@ -0,0 +1,20 @@
+// ********************************************************************************************************
+// Script 		ASA:Publicworks/~/~/~.js
+// Record Types: all
+//
+// Event: 	ASA	
+//
+// Desc:	
+//
+// Created By: Silver Lining Solutions
+// ********************************************************************************************************
+// Change Log
+//         		Date		Name			Modification
+//			08-20-2018		Chad			Initial Draft - tech fee
+// ********************************************************************************************************
+
+logDebug("start ASA:PublicWorks/~/~/~ to add technology fee!");
+
+sumFeesAssessedBeforeInvoiceAndAddTechFee();
+
+logDebug("end  ASA:PublicWorks/~/~/~ to add technology fee!");
