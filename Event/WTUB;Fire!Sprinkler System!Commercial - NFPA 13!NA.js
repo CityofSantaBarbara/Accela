97462f85dca484b87340e41c2bab21cca34c97d1
ejust <42358216+ejust@users.noreply.github.com>
2018-08-21 12:17:05 -0700

    Add files via upload

--- Event/WTUB;Fire!Sprinkler System!Commercial - NFPA 13!NA.js
+++ Event/WTUB;Fire!Sprinkler System!Commercial - NFPA 13!NA.js
@@ -1,5 +1,5 @@
 //********************************************************************************************************
-//Script 		WorkflowTaskUpdateBefore
+//Script 		9 - Fire Sprinkler Monitoring
 //Record Types:	Fire!Sprinkler System!Commercial - NFPA 13!NA 
 //
 //Event: 		WTUB;Fire!Sprinkler System!Commercial - NFPA 13!NA
@@ -14,11 +14,38 @@
 //********************************************************************************************************
 
 
-logDebug("start of WTUB:Fire!Sprinkler System!Commercial - NFPA 13!NA");
+logDebug("start of 9 - Fire Sprinkler Monitoring");
 
 if (wfTask == "Close" && wfStatus == "Closed" ) {
  checkSprinklerHeadAndCancel();
 }
+
+
+
+logDebug("end of 9 - Fire Sprinkler Monitoring");
+
+//********************************************************************************************************
+//Script 		11 - Underground Fireline
+//Record Types:	Fire!Sprinkler System!Commercial - NFPA 13!NA 
+//
+//Event: 		WTUB;Fire!Sprinkler System!Commercial - NFPA 13!NA
+//
+//Desc:			On a FIR sprinkler pmt if the checkbox indicates that an underground fire line is required then there must be 2 separate permits for the fire line.
+ 
+				//Event Trigger: WorkflowTaskUpdateBefore & InspectionResultSubmitBefore (because we are 
+				//trying to automate the workflow from the inspection) 
+				//Record Type(s): Fire Sprinkler – NFPA 13 
+				//Criteria: Task: Task = ‘Inspection’ and status = ‘Final Inspection Complete’ and if checkbox ‘Underground By Others’ = ‘Yes’ 
+				//Actions: verify that 2 related records exists and are closed: Underground Fireline & PBW Fire Line Service 
+//
+//Created By: EJ
+//********************************************************************************************************
+// Change Log
+//         		Date		Name			Modification
+//				08-21-18	EJ				Initial Draft
+//********************************************************************************************************
+logDebug("start of 11 - Underground Fireline");
+
 if (wfTask == "Inspection" && wfStatus == "Final Inspection Complete" ){
 
  showMessage = true;
@@ -26,6 +53,4 @@ if (wfTask == "Inspection" && wfStatus == "Final Inspection Complete" ){
   cancel=true;
  
 }
-
-
-logDebug("end of WTUB:Fire!Sprinkler System!Commercial - NFPA 13!NA");
+logDebug("start of 11 - Underground Fireline");
