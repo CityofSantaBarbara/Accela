454c7d916dd9762fef09ae9676d8447113fae981
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-08-30 09:07:04 -0600

    Add files via upload

--- Event/DUB;~!~!~!~.js
+++ Event/DUB;~!~!~!~.js
@@ -17,8 +17,18 @@
 //				08/15/2018	Eric 			Initial Development
 //********************************************************************************************************
 logDebug("DUB - Begin");
+
 var recordDocArray = null;
+var recDocModel = null;
+
 recordDocArray = getDocumentList();
 logDebug("recordDocArray = " + recordDocArray);
 printObjProperties(recordDocArray);
+
+for (i = 0; i < recordDocArray.length; i++) {
+	recDocModel = recordDocArray[i];
+	logDebug("************* doc Model ****************");
+	printObjProperties(recDocModel);
+}
+
 logDebug("DUB - End");
