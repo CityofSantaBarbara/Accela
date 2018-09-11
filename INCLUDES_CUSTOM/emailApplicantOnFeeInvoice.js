cca320368b5de178f0b1f7ad0bc59c9fac1ccb6e
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-09-11 15:24:08 -0600

    Add files via upload

---
+++ INCLUDES_CUSTOM/emailApplicantOnFeeInvoice.js
@@ -0,0 +1,26 @@
+//********************************************************************************************************
+//Script 		Email Applicant on Fee Invoice
+//Record Types:	​*/*/*/*
+//
+//Event: 		IFA
+//
+//Desc:			When a fee is invoiced let the Applicant know.
+//
+//Assumptions:
+//				If an applicant does not have an email this script will return a warning to the User
+//
+//
+//Created By: Silver Lining Solutions 
+//********************************************************************************************************
+// Change Log
+//         		Date		Name			Modification
+//				09/11/2018	Eric			orig
+//********************************************************************************************************
+function emailApplicantOnFeeInvoice()
+{
+	logDebug("Script 27 Email Applicant on Fee Invoice - Begin");
+
+
+	logDebug("Script 27 Email Applicant on Fee Invoice - End");
+}
+
