580cdfca8ee9f109d227f61e42c3f29592673168
Eric Koontz <EricKoontz@users.noreply.github.com>
2018-09-12 12:28:42 -0600

    Add files via upload

---
+++ Event/ACAA;~!~!~!~.js
@@ -0,0 +1,17 @@
+// ********************************************************************************************************
+// Script 		ACAA:~/~/~/~.js
+// Record Types: all
+//
+// Event: 	ACAA	
+//
+// Desc:	this script is for app submit global actions
+//
+// Created By: Silver Lining Solutions
+// ********************************************************************************************************
+// Change Log
+//         		Date		Name			Modification
+//				9/12/2018	Eric			Orig
+// ********************************************************************************************************
+
+logDebug("ACAA Start");
+logDebug("ACAA End");
---
+++ Event/ACUA;~!~!~!~.js
@@ -0,0 +1,17 @@
+// ********************************************************************************************************
+// Script 		ACUA:~/~/~/~.js
+// Record Types: all
+//
+// Event: 	ACUA	
+//
+// Desc:	this script is for app submit global actions
+//
+// Created By: Silver Lining Solutions
+// ********************************************************************************************************
+// Change Log
+//         		Date		Name			Modification
+//				9/12/2018	Eric			Orig
+// ********************************************************************************************************
+
+logDebug("ACUA Start");
+logDebug("ACUA End");
