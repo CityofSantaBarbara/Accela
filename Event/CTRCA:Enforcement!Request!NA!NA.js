58630519d8938e750bcefaabfa751814c5ecc2d8
AccelaMichaelHe <42595567+AccelaMichaelHe@users.noreply.github.com>
2018-10-17 22:07:57 -0700

    CTRCA for record type Enforcement/Request/NA.NA
    CTRCA event for record type Enforcement/Request/NA.NA

---
+++ Event/CTRCA:Enforcement!Request!NA!NA.js
@@ -0,0 +1,23 @@
+//********************************************************************************************************
+//Script 		Script tracker 37
+//Record Types:	Enforcement!Request!NA!NA 
+//
+//Event: 		CTRCA
+//
+//Desc:			Send an email to inform user.
+//					
+//				Upon application submittal, system will send an email with a user which from an ASI dropdownlist 'Department'.
+//				
+//
+//Created By: Silver Lining Solutions
+//********************************************************************************************************
+// Change Log
+//         		Date		Name			Modification
+//			08-15-2018	Michael			Initial Draft
+//********************************************************************************************************
+
+//logDebug("start of CTRCA:Enforcement!Request!NA!NA");
+
+emailToASIDepartmentUser();
+
+//logDebug("end of CTRCA:Enforcement!Request!NA!NA");
