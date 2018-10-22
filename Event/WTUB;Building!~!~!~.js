3ac9cad5b582e1f57ce2c88c415686f5eb781eb4
wallybot12 <42388330+wallybot12@users.noreply.github.com>
2018-10-22 14:40:01 -0700

    Update WTUB;Building!~!~!~.js

--- Event/WTUB;Building!~!~!~.js
+++ Event/WTUB;Building!~!~!~.js
@@ -17,17 +17,17 @@
 
 logDebug("start of WTUB:Building!~!~!~");
 
-if (wfStatus == "Issued" ) {
-	var thisAddrAttribsArr = [];
-	loadAddressAttributes(thisAddrAttribsArr, capId);
-
-	if (thisAddrAttribsArr["AddressAttribute.OCCUPANCYID"]) {
-		logDebug("you have an occupancy ID on file for the primary address!");
-	} else {
-		logDebug("you DO NOT have an occupancy ID on file for the primary address!");
-		cancel = true;
-		comment("<font color=red><b>You may not issue this permit until an occupancy ID is assigned to the address</b></font>");
-	}
-}
+//if (wfStatus == "Issued" ) {
+//	var thisAddrAttribsArr = [];
+//	loadAddressAttributes(thisAddrAttribsArr, capId);
+//
+//	if (thisAddrAttribsArr["AddressAttribute.OCCUPANCYID"]) {
+//		logDebug("you have an occupancy ID on file for the primary address!");
+//	} else {
+//		logDebug("you DO NOT have an occupancy ID on file for the primary address!");
+//		cancel = true;
+//		comment("<font color=red><b>You may not issue this permit until an occupancy ID is assigned to the address</b></font>");
+//	}
+//}
 
 logDebug("end of WTUB:Building!~!~!~");
