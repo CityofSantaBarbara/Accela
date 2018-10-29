4d4f360f970f7a7f24baaa5895933f064b0128a3
wallybot12 <42388330+wallybot12@users.noreply.github.com>
2018-10-29 09:14:08 -0700

    Update DUA;~!~!~!~.js

--- Event/DUA;~!~!~!~.js
+++ Event/DUA;~!~!~!~.js
@@ -1,1 +1,21 @@
-// emailStaffOnDocUpdate();
+//********************************************************************************************************
+//Script 		Email Staff on Document Update
+//Record Types:	*/*/*/*
+//
+//Event: 		DUA
+//
+//Desc:			When a Revision Required Document gets resubmitted thru ACA. Email the Santa Barbara Staff 
+// 				that is assigned to the Plan Distributed Workflow Task. For BLD cases ONLY, 
+//        Also set the Workflow Task Plans 
+//				Distribution Status to Revisions Received.
+//
+//Assumptions:
+//				Staff must always be assigned to Plans Distribution 
+//				Staff must have a valid email defined in their User Profile
+//
+//Psuedo Code:	
+// 				use Document Update Notification template
+//
+//Created By: Silver Lining Solutions 
+
+emailStaffOnDocUpdate();
