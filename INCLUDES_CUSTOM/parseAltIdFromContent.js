23161d29456db0e29131ed991f81b23763488acb
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-11-09 13:35:15 -0700

    Update parseAltIdFromContent.js

--- INCLUDES_CUSTOM/parseAltIdFromContent.js
+++ INCLUDES_CUSTOM/parseAltIdFromContent.js
@@ -1,13 +1,16 @@
-function parseAltIdFromContent(ctnt)
+function parseAltIdFromContent(content)
 {       
 		//This is just a sample.
 		//Note, please customize the RegExp for actual AlternateID.
-//        var altIdFormat = /Record ID #(.*\w)+/ig; 		
-        var altIdFormat = /RECORD ID #(...\d+-\d+)+/ig;
-		var upContent = ctnt.toUpperCase();
-		var result = altIdFormat.exec(upContent);
+        var altIdFormat = /Record ID #(...\d+-\d+)+/ig;
+//        var altIdFormat = /Record ID #(.*\w)+/; 		this is original from Accela
+		var result = altIdFormat.exec(content);
 		if(result){
 			return result;
 		}
-		aa.print('No record id has been parsed from content.');
+		else {
+			aa.print('No record id has been parsed from content.'+content);
+			logDebug('No record id has been parsed from content. content was:'+content);
+			return null;
+		}
 }
