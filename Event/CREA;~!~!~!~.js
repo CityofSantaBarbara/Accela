d137f5a3cbc972118868cb4f3bf9b618a2a23c9a
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-11-09 14:33:58 -0700

    Update CREA;~!~!~!~.js

--- Event/CREA;~!~!~!~.js
+++ Event/CREA;~!~!~!~.js
@@ -1,3 +1,4 @@
 sendDebugEmail=true;
 debugEmailAddress="chad@esilverliningsolutions.com"
+aa.print("****calling assocate messages to records from CREA/na/na/na!   ******");
 associateMessagesToRecords(messages);
