48c64f25af1ffcbbec82c505bf1349efb901c020
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2018-12-11 17:13:57 -0700

    Update ACUA;~!~!~!~.js

--- Event/ACUA;~!~!~!~.js
+++ Event/ACUA;~!~!~!~.js
@@ -12,9 +12,7 @@
 //         		Date		Name		Modification
 //				09/12/2018	Eric		Orig
 //				12/10/2018	Chad		calling updatePLNConditiontemplateDates();
+//        12/11/2018  Chad    PLN no longer wants updatePLNConditiontemplateDates to run after testing
 // ********************************************************************************************************
 logDebug("START of ACUA */*/*/* !");
-
-updatePLNConditiontemplateDates();
-
 logDebug("END of ACUB */*/*/* !");
