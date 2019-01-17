51bc2f5356dfa562a0cf7b8c9c0213bda0a8fee6
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2019-01-16 21:07:17 -0700

    Add files via upload
    CREATED

---
+++ Pageflow/ACA_PBW_CHECK_FOR_ROW_CONFLICTS.js
@@ -0,0 +1,112 @@
+//********************************************************************************************************
+//Script 		ACA_PBW_CHECK_FOR_ROW_CONFLICTS.js
+//
+//Record Types:	Publicworks
+//
+//Event: 		ACA PAGEFLOW
+//
+//Desc:			
+//
+//Created By: Silver Lining Solutions
+//********************************************************************************************************
+// Change Log
+//            Date        Name			Modification
+//            01-16-2019  Chad			Created
+//********************************************************************************************************
+
+var showMessage = false; // Set to true to see results in popup window
+var showDebug = false; // Set to true to see debug messages in popup window
+var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
+var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
+var cancel = false;
+/*------------------------------------------------------------------------------------------------------/
+| END User Configurable Parameters
+/------------------------------------------------------------------------------------------------------*/
+var startDate = new Date();
+var startTime = startDate.getTime();
+var message = ""; // Message String
+var debug = ""; // Debug String
+var br = "<BR>"; // Break Tag
+
+var SCRIPT_VERSION = 3.0;
+var useCustomScriptFile = true;  // if true, use Events->Custom Script, else use Events->Scripts->INCLUDES_CUSTOM
+var useSA = false;
+var SA = null;
+var SAScript = null;
+var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_FOR_EMSE");
+if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
+    useSA = true;
+    SA = bzr.getOutput().getDescription();
+    bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_INCLUDE_SCRIPT");
+    if (bzr.getSuccess()) {
+        SAScript = bzr.getOutput().getDescription();
+    }
+}
+
+if (SA) {
+    eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA,useCustomScriptFile));
+    eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS_ASB", SA,useCustomScriptFile));
+    eval(getScriptText("INCLUDES_ACCELA_GLOBALS", SA,useCustomScriptFile));
+    eval(getScriptText(SAScript, SA));
+} else {
+    eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useCustomScriptFile));
+    eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS_ASB",null,useCustomScriptFile));
+    eval(getScriptText("INCLUDES_ACCELA_GLOBALS",null,useCustomScriptFile));
+}
+
+eval(getScriptText("INCLUDES_CUSTOM",null,useCustomScriptFile));
+
+function getScriptText(vScriptName, servProvCode, useProductScripts) {
+    if (!servProvCode)  servProvCode = aa.getServiceProviderCode();
+    vScriptName = vScriptName.toUpperCase();
+    var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+    try {
+        if (useProductScripts) {
+            var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
+        } else {
+            var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+        }
+        return emseScript.getScriptText() + "";
+    } catch (err) {
+        return "";
+    }
+}
+var cap = aa.env.getValue("CapModel");
+var parentId = cap.getParentCapID();
+var appTypeResult = cap.getCapType();
+var appTypeString = appTypeResult.toString();           // Convert application type to string ("Building/A/B/C")
+var appTypeArray = appTypeString.split("/");            // Array of application type string
+// page flow custom code begin
+try{
+	comment("START ASA:Publicworks\ROWM\*\* ");
+	checkPBWRightOfWayConflicts();
+	comment("END ASA:Publicworks\ROWM\*\* ");
+	cancel=true;
+}
+catch(err){
+    //cancel = true;
+    //showDebug = 3;
+    logDebug("Error on custom pageflow ACA_FIRE_LOC_AND_CONTACT_INFO_BEFORE. Err: " + err);
+}
+
+// page flow custom code end
+
+
+if (debug.indexOf("**ERROR") > 0) {
+    aa.env.setValue("ErrorCode", "1");
+    aa.env.setValue("ErrorMessage", debug);
+} else {
+    if (cancel) {
+        aa.env.setValue("ErrorCode", "-2");
+        if (showMessage)
+            aa.env.setValue("ErrorMessage", message);
+        if (showDebug)
+            aa.env.setValue("ErrorMessage", debug);
+    } else {
+        aa.env.setValue("ErrorCode", "0");
+        if (showMessage)
+            aa.env.setValue("ErrorMessage", message);
+        if (showDebug)
+            aa.env.setValue("ErrorMessage", debug);
+    }
+}
