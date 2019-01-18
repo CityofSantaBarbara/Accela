ce696d7432e16d9a628173cfb9a8b2516c1426eb
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2019-01-17 17:30:34 -0700

    Update getROWOverlapStreetRecords.js
    aca changes

--- INCLUDES_CUSTOM/getROWOverlapStreetRecords.js
+++ INCLUDES_CUSTOM/getROWOverlapStreetRecords.js
@@ -13,6 +13,7 @@
 //            Date        Name			Modification
 //            01-16-2019  Chad			Created
 //	      01-16-2019  Chad                  changed to check for b1_alt_id match
+//			01-17-2019	Chad			Changes for ACA
 //********************************************************************************************************
 function getROWOverlapStreetRecords( lStartDate, lEndDate, lStreetName, lStreetStartNum, lStreetEndNum ) {
 logDebug("START getROWOverlapStreetRecords");
@@ -30,6 +31,8 @@ logDebug("START getROWOverlapStreetRecords");
 		logDebug("SQL Success!!!");
 		var cntr = 0;
 
+		var chkAltId = "";
+
 		while (rSet.next()) {
 /* Depending on what we really need here we could list out the rec IDs, OR just set a boolean and be done on the first pass */
 			var mB1_Alt_id	= rSet.getString("B1_ALT_ID"); 
@@ -40,7 +43,8 @@ logDebug("START getROWOverlapStreetRecords");
 			var mEndNum		= rSet.getString("EndNum"); 
 
 						
-			if (mB1_Alt_id != cap.getCapModel().getAltID()) {
+			if (!publicUser) chkAltId = cap.getCapModel().getAltID();	
+			if (mB1_Alt_id != chkAltId) {
 				matchedRecList.push(mB1_Alt_id);
 				var msgStr	=	"Potential Right of Way Work Conflicts at record:"+mB1_Alt_id
 							+	"<br>      starting:"+mWorkStart
@@ -58,7 +62,7 @@ logDebug("START getROWOverlapStreetRecords");
 		logDebug("found :"+cntr+" records that are potential conflicts!");
 	}
 	catch(err){
-		aa.print("Error on GoGetAnSQLDone function. Please contact administrator. Err: " + err);
+		logDebug("Error on GoGetAnSQLDone function. Please contact administrator. Err: " + err);
 	}
 //	logDebug("your rec list is:"+matchedRecList);
 	var unqRecMatches = uniqArray(matchedRecList);
