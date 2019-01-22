8cca62d2933c6afd29f318c55a40e2d34ca4089e
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2019-01-22 14:23:51 -0700

    Update ASA;Publicworks!ROWM!~!~.js
    Moved logic to ASA:Publicworks!~!~!~

--- Event/ASA;Publicworks!ROWM!~!~.js
+++ Event/ASA;Publicworks!ROWM!~!~.js
@@ -12,23 +12,7 @@
 // Change Log
 //            Date        Name			Modification
 //            01-16-2019  Chad			Created
+//	01-22-2019		Chad		Moved logic to ASA:Publicworks!~!~!~
 //********************************************************************************************************
 logDebug("START ASA:Publicworks\ROWM\*\* ");
-checkPBWRightOfWayConflicts();
-
-	if(!publicUser){
-		tpbwRowAddresses = loadASITable("PBW_ROWADDRESS", capId);
-	}
-	else {
-		tpbwRowAddresses = loadASITable4ACA("PBW_ROWADDRESS", capId);
-	}
-	
-	if (tpbwRowAddresses) {
-		var gisAddSearchType = 'PARCEL';
-		mapGISStreetSegsFromROWMASIT(tpbwRowAddresses,gisAddSearchType);
-	}
-	else { 
-		logDebug("no PBW_ROWADDRESS table information exists on this record:"+capId);
-	}
-
 logDebug("END ASA:Publicworks\ROWM\*\* ");
