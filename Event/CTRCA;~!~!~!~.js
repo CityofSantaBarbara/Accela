575db106851dd1e85aea9829a02531e423356ed3
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2019-02-12 14:26:58 -0700

    Update CTRCA;~!~!~!~.js
    UAT issue 319

--- Event/CTRCA;~!~!~!~.js
+++ Event/CTRCA;~!~!~!~.js
@@ -12,6 +12,7 @@
 //         		Date		Name			Modification
 //				      Jason			Initial Draft
 //			08-08-2018	Chad			changed header
+//                02-12-2018  Chad              Fixed UAT issue 319 by adding exp logic here.
 //********************************************************************************************************
       
       
@@ -23,3 +24,33 @@ try{
       logDebug("A JavaScript Error occurred: CTRCA:~/~/~/~: copyParcelGisObjects()" + err.message);
       logDebug(err.stack);
 };
+
+
+var thisExpModelList = aa.expiration.getLicensesByCapID(capId);
+
+if (thisExpModelList.getSuccess()) {
+	var thisExpModel = thisExpModelList.getOutput();
+	var thisExp = thisExpModel.getB1Expiration();
+	if (thisExp != null ) { 
+		var thisExpCurrentStatus = thisExpModel.getExpStatus();
+		if (!thisExpCurrentStatus || thisExpCurrentStatus == "" ) {
+			thisExpModel.setExpStatus("Active");
+			thisB1ExpModel = thisExpModel.getB1Expiration();
+			var result = aa.expiration.editB1Expiration(thisB1ExpModel);
+
+			var daysToAdd = lookup("INITIAL_RECORD_EXPIRES_IN_DAYS",appTypeArray[0]);
+			if (daysToAdd) {
+				var tmpDate = new Date();
+				var newExpDate = dateAdd(tmpDate,daysToAdd);
+				
+				thisExpModel.setExpDate(aa.date.parseDate(newExpDate));
+				var result = aa.expiration.editB1Expiration(thisExpModel.getB1Expiration());
+				comment("setting active AND setting expire date to:"+newExpDate);
+
+				// update asi 
+				logDebug("updating ASI Application Expiration Date to:" + newExpDate);
+				editAppSpecific("Application Expiration Date",newExpDate);
+			}
+		} 
+	}
+}
