178050e20c3f02e596b2c94af3076076f3a1ab56
Eric Koontz <EricKoontz@users.noreply.github.com>
2019-02-18 21:59:48 -0700

    Add files via upload

--- INCLUDES_CUSTOM/notifation.js
+++ INCLUDES_CUSTOM/notifation.js
@@ -105,7 +105,7 @@ function notificationParamBuild(emailParameters)
 	addParameter(emailParameters, "$$altID$$", cap.getCapModel().getAltID());
 	addParameter(emailParameters, "$$recordAlias$$", cap.getCapType().getAlias());
 	addParameter(emailParameters, "$$wfComments$$", wfComment);
-	addParameter(emailParameters, "$$recAddress$$", getAddress(capId);
+	addParameter(emailParameters, "$$recAddress$$", getAddress(capId));
 	
 	return emailParameters;
 }
