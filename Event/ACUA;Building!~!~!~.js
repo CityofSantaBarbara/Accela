56d0df1e71d8d6c665007760176ab72903bb9030
Eric Koontz <EricKoontz@users.noreply.github.com>
2019-06-18 09:18:07 -0600

    Add files via upload

---
+++ Event/ACUA;Building!~!~!~.js
@@ -0,0 +1,21 @@
+// ********************************************************************************************************
+// Script 		ACUA:Building/~/~/~.js
+// Record Types: all
+//
+// Event: 	ACAA	
+//
+// Desc:	this script is for app submit global actions
+//
+// Created By: Silver Lining Solutions
+// ********************************************************************************************************
+// Change Log
+//         		Date		Name		Modification
+//				06/18/2018	Eric		Orig
+//
+// ********************************************************************************************************
+logDebug("START of ACUA Building/*/*/* !");
+
+logDebug("Condition Type = " + conditionType);
+logDebug("Condition Status = " + conditionStatus);
+
+logDebug("END of ACUA Building/*/*/* !");
