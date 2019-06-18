918c8bf72aad25bdb0daa8d6b96a44bb53d14717
Eric Koontz <EricKoontz@users.noreply.github.com>
2019-06-18 15:15:50 -0600

    Add files via upload

---
+++ INCLUDES_CUSTOM/closeAssociatedPlanReviewTask.js
@@ -0,0 +1,42 @@
+// ********************************************************************************************************
+// Script 		closeAssociatedPlanReviewTask.js
+// Record Types: all
+//
+// Event: 	ACAA	
+//
+// Desc:	for plan reviews being completed using the conditions, close the associated WF task
+//
+// Created By: Silver Lining Solutions
+// ********************************************************************************************************
+// Change Log
+//         		Date		Name		Modification
+//				06/18/2018	Eric		Orig
+//
+// ********************************************************************************************************
+
+function closeAssociatedPlanReviewTask()
+{
+	var tempStr = conditionObj.getDispConditionDescription().toString();
+	var condDesc = "" + tempStr.toString();
+	var lenDesc = condDesc.length;
+	var startPos = condDesc.indexOf(":");
+	var lookupValue = condDesc.substr(startPos+2,lenDesc);
+
+	logDebug("******* Condition Info *********************");
+	logDebug("       Condition Type = " + conditionType);
+	logDebug("	   Condition Status = " + conditionStatus);
+	logDebug("Condition Description = " + condDesc);
+	logDebug("				 length = " + lenDesc);
+	logDebug("             startPos = " + startPos);
+	logDebug("          lookupValue = " + lookupValue);
+
+	var task = lookup("BLD_CONDITION_WFTASK_MAP", lookupValue);
+	logDebug("                 task = " + task);
+
+	if (task != -1 && isTaskActive(task)){	
+		logDebug("the associated task is active");
+		closeTask(task,conditionStatus,"Associated Condition updated","Associated Condition updated");
+		}
+		
+	//printObjProperties(conditionObj);
+}
---
+++ INCLUDES_CUSTOM/createConditionForPlanReview.js
@@ -0,0 +1,43 @@
+// ********************************************************************************************************
+// Script 		createConditionForPlanReview.js
+// Record Types: all
+//
+// Event: 	ACAA	
+//
+// Desc:	for plan reviews being set to Routed for Review, create the associated Condition
+//
+// Created By: Silver Lining Solutions
+// ********************************************************************************************************
+// Change Log
+//         		Date		Name		Modification
+//				06/18/2018	Eric		Orig
+//
+// ********************************************************************************************************
+
+function createConditionForPlanReview()
+{
+	logDebug("START of createConditionForPlanReview");
+	
+	
+	/* handle the counter */
+	if (wfTask == "Plans Distribution" && wfStatus == "Routed for Review") {
+		logDebug("Matched on Plans Distribution & Routed for Review");
+		var prdCount = parseInt(getAppSpecific("Plan Review Distribution Count"));
+		editAppSpecific("Plan Review Distribution Count", prdCount + 1);
+	}
+	
+	/* create the associated plan review */
+	var conditionDesc = lookup("BLD_WFTASK_CONDITION_MAP",wfTask);
+	if (if conditionDesc != -1 && wfStatus == "Routed to Reviewer") {
+
+		var prdCount = getAppSpecific("Plan Review Distribution Count");	
+		var title = "Review " + prdCount + ": " + conditionDesc;
+		var newStatus = "Routed to Reviewer " + prdCount;
+		logDebug("Matched on " + conditionDesc + " " & Routed & title =" + title + " & prdCount = " + prdCount);
+		addAppCondition("Plan Review","Pending(Applied)",title,"01025","Notice");
+		updateTask(wfTask,newStatus,"comment","note");
+	}
+	
+	logDebug("END of createConditionForPlanReview");
+}
+
