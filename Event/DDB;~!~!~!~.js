2c6b30073cdaeac0b54d9d9720806d9994267222
Alec Hardy <42388330+wallybot12@users.noreply.github.com>
2019-06-19 14:35:02 -0700

    Update DDB;~!~!~!~.js

--- Event/DDB;~!~!~!~.js
+++ Event/DDB;~!~!~!~.js
@@ -1,13 +1,16 @@
 showMessage = true;
 validateDocument();
 
-function validateDocument(){
-if (typeof(docCapStatus) != 'undefined')
+function validateDocument()
+{
+
+if (typeof(docCapStatus) != "undefined")
       {
 	//aa.print("capstatus="+docCapStatus );
-	if(docCapStatus && (docCapStatus == "Ready to Issue" || docCapStatus == "Revisions Required")){
+	if(docCapStatus && (docCapStatus == "Ready to Issue" || docCapStatus == "Revisions Required"))
+		{
 		cancel = true;
-		comment(" You can not edit the document because current records status is : " + docCapStatus);
+		aa.print(" You can not edit the document because current records status is : " + docCapStatus);
 	        }
       }
 	
