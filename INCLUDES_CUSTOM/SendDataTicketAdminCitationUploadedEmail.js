919664c485a3fd8be6da3200b78d568e45e0ac9e
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2019-07-10 21:22:21 -0600

    Create SendDataTicketAdminCitationUploadedEmail.js

---
+++ INCLUDES_CUSTOM/SendDataTicketAdminCitationUploadedEmail.js
@@ -0,0 +1,121 @@
+//********************************************************************************************************
+//Script 		SendDataTicketAdminCitationUploadedEmail 
+//Record Types:	​Enforcement/*/*/*
+//
+//Event: 		this script may be triggered from DocumentUploadAfter.
+//
+//Desc:			When a user uploads a document that has "ADMIN CITATION" in its name, send 
+//				dataticket.com an email notification and attach the new uploaded file.
+//
+//Created By: Silver Lining Solutions 
+//********************************************************************************************************
+// Change Log
+//				Date		Name			Modification
+//				06/24/2018	Chad			Original Development
+//********************************************************************************************************
+function SendDataTicketAdminCitationUploadedEmail()
+{
+	var toEmail = null; //lookup("ADMIN_CITATION_UPLOADED_EMAIL", "EMAIL_TO");
+
+	var fromEmail = scriptAgencyEmailFrom;
+//	var fromEmail = 'AccelaDev@santabarbaraca.gov';
+	var ccEmail = "";
+	var notificationTemplate = "ADMIN CITATION UPLOADED";
+	var reportFile = [];  // empty set for the file list
+	var capID4Email = aa.cap.createCapIDScriptModel(capId.getID1(),capId.getID2(),capId.getID3());
+	var emailParameters = aa.util.newHashtable();
+	var staff = null;
+	var lAltId = cap.getCapModel().getAltID();
+	var lRecAlias = cap.getCapType().getAlias();
+
+//	toEmail += " admincites@dataticket.com   ROLL THIS WHEN YOU ARE READY TO ACTUALL TEST
+//	toEmail += "cweiffenbach@santabarbaraca.gov";
+	
+
+	//	spec says no aca as of 6-11-19
+	//	var acaSite = lookup("ACA_CONFIGS", "OFFICIAL_WEBSITE_URL");
+	//	addParameter(emailParameters,"$$acaUrl$$",acaSite);
+
+
+	// identify the doc(s) that were just uploaded and for each doc, send a notification
+	//chad add this line back when you are ready to roll
+	//	var docArray = documentModelArray.toArray();
+	// fake it out here
+	docArray = documentModelArray.toArray();
+	var err = 0;
+
+	var documentModel = null;
+	var fileName = null;
+
+	// find the doc uploaded that has 'Appeal Request%' in the doc name 
+	// - talk to biz about this, might be better to have docd type
+	logDebug("document model array length is :"+docArray.length);
+	for (i = 0; i < docArray.length; i++) {
+		documentModel = docArray[i];
+		var iDocFileName = documentModel.getDocName();
+		iDocFileName = iDocFileName.toUpperCase();
+
+		var iDocCat			= documentModel.getDocCategory();
+		var iDocGroup		= documentModel.getDocGroup();
+		
+		// lateset criteria from Andrew 06/28/19
+		// 		group must be 'ENF', doc cats must be in:
+		//		"Admin Citation 1", 
+		//		"Admin Citation 2", 
+		//		"Admin Citation 3 or More", 
+		//		"Admin Citation Appeal Request"
+		
+		
+//		if (iDocFileName.indexOf("ADMIN CITATION") >= 0 ) {  // FOUND IT!
+		if (iDocGroup == 'ENF' && (
+				idocCat == 'Admin Citation 1' ||
+				idocCat == 'Admin Citation 2' ||
+				idocCat == 'Admin Citation 3 or More' ||
+				idocCat == 'Admin Citation Appeal Request'
+			))
+		{
+		
+			var iDocCustomID	= lAltId;
+			var iDocEntityID	= documentModel.getEntityID();
+			var iDocEntityType	= documentModel.getEntityType();
+			var iDocType		= documentModel.getDocType();
+			var iDocID			= documentModel.getDocumentNo();
+			var iDocFileKey		= documentModel.getFileKey();
+			var iDocFileName	= documentModel.getFileName();
+			var iDocFileSize	= documentModel.getFileSize();
+			var iDocUploadedBy	= documentModel.getFileUpLoadBy();
+			var iDocUploadDate	= documentModel.getFileUpLoadDate();
+			var iDocSource		= documentModel.getSource();
+
+			var iDocFileName2 = encodeURI(iDocFileName);
+			logDebug("doc name is:"+iDocFileName);
+			logDebug("doc name uri:"+iDocFileName2);
+
+			// prepare Notification parameters
+			addParameter(emailParameters, "$$altID$$", lAltId);
+			addParameter(emailParameters, "$$recordAlias$$", lRecAlias);
+			addParameter(emailParameters, "$$docNo$$", iDocID);
+			addParameter(emailParameters, "$$docType$$", iDocType);
+			addParameter(emailParameters, "$$docGroup$$", iDocGroup);
+			addParameter(emailParameters, "$$docFileName$$", iDocFileName);
+			addParameter(emailParameters, "$$docName$$", iDocFileName);
+			addParameter(emailParameters, "$$docCategory$$", iDocCat);
+			addParameter(emailParameters, "$$docUploadBy$$", iDocUploadedBy);
+			addParameter(emailParameters, "$$docUploadDate$$", iDocUploadDate);
+
+
+			var iFilePathResult = aa.document.downloadFile2Disk(documentModel, documentModel.getModuleName(),"","",true);
+			if(iFilePathResult.getSuccess())
+			{
+				var iFilePath = iFilePathResult.getOutput();
+				logDebug("the file path is :"+iFilePath);
+				reportFile.push(iFilePath);
+				var sendResult = sendNotification(fromEmail,toEmail,ccEmail,notificationTemplate,emailParameters,reportFile,capID4Email);
+				if (!sendResult) 
+					{ logDebug("UNABLE TO SEND NOTICE!  ERROR: "+sendResult); }
+				else
+					{ logDebug("Sent Notification"); }  
+			}
+		}
+	}
+}
