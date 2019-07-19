6b704c9005ceea61b038ab9417cae960253649ad
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2019-07-19 09:23:48 -0600

    Update WTUA;Enforcement!~!~!~.js
    Added scripting for Issue 50

--- Event/WTUA;Enforcement!~!~!~.js
+++ Event/WTUA;Enforcement!~!~!~.js
@@ -1,2 +1,6 @@
 // stub out WTUA for enforcement records
 cloneREQToENF();
+if ( wfTask == "Investigation Findings" && wfStatus == "2.2 Yes-NOV Published") {
+	var staffAsgnd = getAssignedStaff();
+	scheduleInspection("Enforcement Follow-up",30,staffAsgnd,null,"workflow changed to 2.2 Yes-NOV Published.  Follow-up needed");
+}
