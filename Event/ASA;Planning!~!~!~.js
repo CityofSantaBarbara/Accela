0704d9aa71f584f2f50baa09eabd0fd824a5e7de
ChadSLS <24942229+ChadSLS@users.noreply.github.com>
2019-10-18 01:09:25 -0600

    Add files via upload

---
+++ Event/ASA;Planning!~!~!~.js
@@ -0,0 +1,17 @@
+// ********************************************************************************************************
+// Script 		ASA:Planning/~/~/~.js
+// Record Types: all
+//
+// Event: 	ASA	
+//
+// Desc:	this script is for app submit global actions
+//
+// Created By: Silver Lining Solutions
+// ********************************************************************************************************
+// Change Log
+//         	Date		Name			Modification
+//			10-17-2019		Chad			Original
+// ********************************************************************************************************
+logDebug("Start of ASA:Planning/*/*/*");
+updateAppNameWithAddress();
+logDebug("End of ASA:Planning/*/*/*");
