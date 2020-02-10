057f8a54618cf5c33bf841cc5688653f348982bb
grayquarter <john@grayquarter.com>
2020-02-10 14:39:17 -0800

    inspection area assignments

--- Event/ISA;Building!~!~!~.js
+++ Event/ISA;Building!~!~!~.js
@@ -1,9 +1,9 @@
 
 var inspAssignArea = String(getGISInfo(aa.getServiceProviderCode(), "Building Permit Inspection Areas", "Region", "-1"));
 
-if (inspId && inspAssignArea.length() > 0) {
+if (inspId && inspAssignArea.length > 0) {
 	var inspectorId = String(lookup("BLD_INSP_ASSIGNMENT_MAPPING", inspAssignArea));
-	if (inspectorId.length() > 0) {
+	if (inspectorId.length > 0) {
 		assignInspection(inspId, inspectorId);
 	}
 }
