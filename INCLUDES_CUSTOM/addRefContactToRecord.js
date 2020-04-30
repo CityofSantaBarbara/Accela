601da68bbd7ec7b465da7d5a7cac5d27ba2f09e6
grayquarter <john@grayquarter.com>
2020-04-30 13:43:10 -0700

    zen # 254

---
+++ INCLUDES_CUSTOM/addRefContactToRecord.js
@@ -0,0 +1,23 @@
+function addRefContactToRecord(refNum, cType) {
+    itemCap = capId;
+    if (arguments.length > 2)
+        itemCap = arguments[2];
+    var refConResult = aa.people.getPeople(refNum);
+    if (refConResult.getSuccess()) {
+        var refPeopleModel = refConResult.getOutput();
+        if (refPeopleModel != null) {
+            pm = refPeopleModel;
+            pm.setContactType(cType);
+            pm.setFlag("N");
+            pm.setContactAddressList(getRefAddContactList(refNum));
+            
+            // var result = aa.people.createCapContactWithRefPeopleModel(itemCap, pm);
+            if (result.getSuccess()) {
+                logDebug("Successfully added the contact");
+            }   
+            else {
+                logDebug("Error creating the applicant " + result.getErrorMessage());
+            }
+        }
+    }
+}
