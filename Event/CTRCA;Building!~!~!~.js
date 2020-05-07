568a5a8c7f2239fb6c3f3e6d0ad6e3211251c2b1
grayquarter <john@grayquarter.com>
2020-05-07 14:33:17 -0700

    Update CTRCA;Building!~!~!~.js

--- Event/CTRCA;Building!~!~!~.js
+++ Event/CTRCA;Building!~!~!~.js
@@ -1,3 +1,20 @@
 //Added by Gray Quarter per request of Andrew on 4/29/2020 and tracked in zen #500 sharepoint 254
 
-addRefContactToRecord(183,"Contact");
+try {
+	addRefContactToRecord(183,"Contact");
+	
+	var acaFeeMap = getFeeRecsToProcess();
+
+for (var i in acaFeeMap) {
+	// add by record type
+		var m = acaFeeMap[i];
+		for (var j in m.recType) {
+			//logDebug("inserting fee for " + m.recType[j]);
+			if (appMatch(String(m.recType[j])))
+			{
+				updateFee(m.feeCode, m.feeSchedule,"FINAL",m.feeAmount, "N");
+			}
+		}
+	}
+}
+catch(err) { aa.print("An error occured trying to assess an ACA fee: " + err.message); }
