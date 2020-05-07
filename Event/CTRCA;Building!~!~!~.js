bf5f18caf9821e7be0d9aa5ad91ce5d9c48b90ea
grayquarter <john@grayquarter.com>
2020-05-07 15:13:32 -0700

    removed script

--- Event/CTRCA;Building!~!~!~.js
+++ Event/CTRCA;Building!~!~!~.js
@@ -1,20 +1,3 @@
 //Added by Gray Quarter per request of Andrew on 4/29/2020 and tracked in zen #500 sharepoint 254
 
-try {
-	addRefContactToRecord(183,"Contact");
-	
-	var acaFeeMap = getFeeRecsToProcess();
-
-for (var i in acaFeeMap) {
-	// add by record type
-		var m = acaFeeMap[i];
-		for (var j in m.recType) {
-			//logDebug("inserting fee for " + m.recType[j]);
-			if (appMatch(String(m.recType[j])))
-			{
-				updateFee(m.feeCode, m.feeSchedule,"FINAL",m.feeAmount, "N");
-			}
-		}
-	}
-}
-catch(err) { aa.print("An error occured trying to assess an ACA fee: " + err.message); }
+addRefContactToRecord(183,"Contact");
