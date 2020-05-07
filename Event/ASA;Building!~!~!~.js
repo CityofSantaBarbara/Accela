51c6fa605f398b8990871328dcf6778082307df3
grayquarter <john@grayquarter.com>
2020-05-07 15:22:17 -0700

    Added changes for zen#493

--- Event/ASA;Building!~!~!~.js
+++ Event/ASA;Building!~!~!~.js
@@ -1,3 +1,20 @@
 //Added by Gray Quarter per request of Andrew on 4/29/2020 and tracked in zen #500 sharepoint 254
 
 addRefContactToRecord(183,"Contact");
+
+// Added by Gray Quarter for zen #493
+if (publicUser) {  
+var acaFeeMap = getFeeRecsToProcess();
+
+for (var i in acaFeeMap) {
+	// add by record type
+		var m = acaFeeMap[i];
+		for (var j in m.recType) {
+			//logDebug("inserting fee for " + m.recType[j]);
+			if (appMatch(String(m.recType[j])))
+			{
+				updateFee(m.feeCode, m.feeSchedule,"FINAL",m.feeAmount, "N");
+			}
+		}
+	}
+}
