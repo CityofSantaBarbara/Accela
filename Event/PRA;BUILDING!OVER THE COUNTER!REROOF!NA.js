239e75f9e8c2365dd7f0b77d61cf29b8f7b8bfe1
jason-grayquarter <64280334+jason-grayquarter@users.noreply.github.com>
2020-07-23 13:08:00 -0700

    Add files via upload

---
+++ Event/PRA;BUILDING!OVER THE COUNTER!REROOF!NA.js
@@ -0,0 +1,8 @@
+//PRA;BUILDING!OVER THE COUNTER!REROOF!NA.js
+//Added by Gray Quarter 
+//New On Demand Re-Roof record for ACA
+
+if(isPublicUser) { 
+    closeTask("Application Submittal","Ready to Issue"); 
+    closeTask("Permit Issuance","Issued"); 
+    }
