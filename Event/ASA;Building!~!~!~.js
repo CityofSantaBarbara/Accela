395d55727cd5712681398d790df33a3fa3372454
grayquarter <john@grayquarter.com>
2020-10-15 15:55:45 -0700

    update for zen 1106

--- Event/ASA;Building!~!~!~.js
+++ Event/ASA;Building!~!~!~.js
@@ -1,6 +1,7 @@
 //Added by Gray Quarter per request of Andrew on 4/29/2020 and tracked in zen #500 sharepoint 254
 if (!publicUser) {  
-addRefContactToRecord(183,"Contact");
+	addRefContactToRecord(183,"Contact");
+	addRefContactToRecord(212,"Business Owner"); // added for zen 1106
 }
 
 // Added by Gray Quarter for zen #493
