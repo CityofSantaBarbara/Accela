99b2ec6e82975f86150ae972b1a8629be3f89b78
grayquarter <john@grayquarter.com>
2020-10-15 15:56:42 -0700

    Added for zen 1106

--- Event/CTRCA;Building!~!~!~.js
+++ Event/CTRCA;Building!~!~!~.js
@@ -1,3 +1,4 @@
 //Added by Gray Quarter per request of Andrew on 4/29/2020 and tracked in zen #500 sharepoint 254
 
 addRefContactToRecord(183,"Contact");
+addRefContactToRecord(212,"Business Owner");  // added for zen 1106
